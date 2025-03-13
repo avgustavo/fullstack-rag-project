08dbe8e2a32ae01397d01f374ec39065d3857c94
Gustavo Vieira de Andrade <avgustavo01@gmail.com>
2025-03-13 13:37:06 +0000

    Add HF and Groq search Filter routes

--- backend/src/controllers/llm/swagger.ts
+++ backend/src/controllers/llm/swagger.ts
@@ -12,7 +12,8 @@ class LLMSwagger {
 
   public swagger = {
     groqSearchV1: {
-      summary: "Recuperação de geração utilizando o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1'",
+      summary:
+        "Recuperação de geração utilizando o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1'",
       operationId: "groqSearchV1",
       tags: ["Groq"],
       requestBody: {
@@ -20,7 +21,7 @@ class LLMSwagger {
         required: true,
         content: {
           "application/json": {
-            schema: this.schemas.searchBody
+            schema: this.schemas.searchBody,
           },
         },
       },
@@ -38,7 +39,8 @@ class LLMSwagger {
       },
     },
     groqSearchV2: {
-      summary: "Recuperação de geração utilizando o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1.5'",
+      summary:
+        "Recuperação de geração utilizando o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1.5'",
       operationId: "groqSearchV2",
       tags: ["Groq"],
       requestBody: {
@@ -46,7 +48,169 @@ class LLMSwagger {
         required: true,
         content: {
           "application/json": {
-            schema: this.schemas.searchBody
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    groqFilterSearchV1: {
+      summary:
+        "Recuperação de geração com FILTROS utilizando o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1'",
+      operationId: "groqFilterSearchV1",
+      tags: ["Groq-Filtrado"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    groqFilterSearchV2: {
+      summary:
+        "Recuperação de geração com FILTROS o modelo 'Groq' e o modelo de embbeding 'nomic-embed-text-v1.5'",
+      operationId: "groqFilterSearchV2",
+      tags: ["Groq-Filtrado"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    hfSearchV1: {
+      summary:
+        "Recuperação de geração utilizando o modelo 'hf' e o modelo de embbeding 'nomic-embed-text-v1'",
+      operationId: "hfSearchV1",
+      tags: ["Hugging Face"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    hfSearchV2: {
+      summary:
+        "Recuperação de geração utilizando o modelo 'hf' e o modelo de embbeding 'nomic-embed-text-v1.5'",
+      operationId: "hfSearchV2",
+      tags: ["Hugging Face"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    hfFilterSearchV1: {
+      summary:
+        "Recuperação de geração com FILTROS utilizando o modelo 'hf' e o modelo de embbeding 'nomic-embed-text-v1'",
+      operationId: "hfFilterSearchV1",
+      tags: ["HF-Filtrado"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
+          },
+        },
+      },
+      responses: {
+        "200": {
+          description: "Pergunta respondida com sucesso",
+          content: {
+            "application/json": {
+              schema: this.schemas.searchLogRes,
+            },
+          },
+        },
+        "400": { description: "Parâmetros inválidos" },
+        "500": { description: "Erro interno do servidor" },
+      },
+    },
+    hfFilterSearchV2: {
+      summary:
+        "Recuperação de geração com FILTROS o modelo 'hf' e o modelo de embbeding 'nomic-embed-text-v1.5'",
+      operationId: "hfFilterSearchV2",
+      tags: ["HF-Filtrado"],
+      requestBody: {
+        description: "Recebe a pergunta do usuário",
+        required: true,
+        content: {
+          "application/json": {
+            schema: this.schemas.searchBody,
           },
         },
       },
@@ -72,7 +236,24 @@ class LLMSwagger {
     [`${this.route}/groqv2`]: {
       post: this.swagger.groqSearchV2,
     },
-
+    [`${this.route}/groqv1-filter`]: {
+      post: this.swagger.groqFilterSearchV1,
+    },
+    [`${this.route}/groqv2-filter`]: {
+      post: this.swagger.groqFilterSearchV2,
+    },
+    [`${this.route}/hfv1`]: {
+      post: this.swagger.hfSearchV1,
+    },
+    [`${this.route}/hfv2`]: {
+      post: this.swagger.hfSearchV2,
+    },
+    [`${this.route}/hfv1-filter`]: {
+      post: this.swagger.hfFilterSearchV1,
+    },
+    [`${this.route}/hfv2-filter`]: {
+      post: this.swagger.hfFilterSearchV2,
+    },
   };
 }
 
