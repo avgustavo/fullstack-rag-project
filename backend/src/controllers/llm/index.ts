6192ad146f95fc80e01d9e2df93609f864040f23
Gustavo Vieira de Andrade <avgustavo01@gmail.com>
2025-03-13 13:38:12 +0000

    Add other routes

--- backend/src/controllers/llm/index.ts
+++ backend/src/controllers/llm/index.ts
@@ -1,11 +1,11 @@
 // src/controllers/syllabus/syllabus.controller.ts
 import { Request, Response, Router } from "express";
 import { searchBody, searchLogRes } from "./schema";
-import {SearchService} from "../../services/search.service";
+import { SearchService } from "../../services/search.service";
 
 class LLMController {
   public router: Router;
-  
+
   constructor(private readonly searchService: SearchService) {
     this.router = Router();
     this.routes();
@@ -15,12 +15,12 @@ class LLMController {
   public groqSearchV1 = async (req: Request, res: Response): Promise<Response> => {
     try {
       // Validação dos dados enviados (incluindo fileUrl)
-      const {prompt} = searchBody.parse(req.body);
-      console.log('recebido o prompt')
+      const { prompt } = searchBody.parse(req.body);
+      console.log("recebido o prompt");
 
       // Chama o service para realizar a ingestão da ementa
       const logs = await this.searchService.searchRAG(prompt, "groq", "nomic-embed-text-v1");
-      
+
       // Valida a resposta usando o schema de saída
       const parsedResult = searchLogRes.parse(logs);
       return res.status(200).json(parsedResult);
@@ -33,11 +33,11 @@ class LLMController {
   public groqSearchV2 = async (req: Request, res: Response): Promise<Response> => {
     try {
       // Validação dos dados enviados (incluindo fileUrl)
-      const {prompt} = searchBody.parse(req.body);
+      const { prompt } = searchBody.parse(req.body);
 
       // Chama o service para realizar a ingestão da ementa
       const logs = await this.searchService.searchRAG(prompt, "groq", "nomic-embed-text-v1.5");
-      
+
       // Valida a resposta usando o schema de saída
       const parsedResult = searchLogRes.parse(logs);
       return res.status(200).json(parsedResult);
@@ -47,6 +47,110 @@ class LLMController {
     }
   };
 
+  public groqFilterSearchV1 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+      console.log("recebido o prompt");
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.filterSearch(prompt, "groq", "nomic-embed-text-v1");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+  public groqFilterSearchV2 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.filterSearch(prompt, "groq", "nomic-embed-text-v1.5");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+  public hfSearchV1 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+      console.log("recebido o prompt");
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.searchRAG(prompt, "hf", "nomic-embed-text-v1");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+  public hfSearchV2 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.searchRAG(prompt, "hf", "nomic-embed-text-v1.5");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+  public hfFilterSearchV1 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+      console.log("recebido o prompt");
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.filterSearch(prompt, "hf", "nomic-embed-text-v1");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
+
+  public hfFilterSearchV2 = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      // Validação dos dados enviados (incluindo fileUrl)
+      const { prompt } = searchBody.parse(req.body);
+
+      // Chama o service para realizar a ingestão da ementa
+      const logs = await this.searchService.filterSearch(prompt, "hf", "nomic-embed-text-v1.5");
+
+      // Valida a resposta usando o schema de saída
+      const parsedResult = searchLogRes.parse(logs);
+      return res.status(200).json(parsedResult);
+    } catch (error: any) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+  };
 
   public routes() {
     // Define a rota POST para ingestão das ementas
@@ -55,6 +159,26 @@ class LLMController {
     });
     this.router.post("/llm/groqv2", async (req, res) => {
       await this.groqSearchV2(req, res);
+    });
+    // Define a rota POST para ingestão das ementas
+    this.router.post("/llm/groqv1-filter", async (req, res) => {
+      await this.groqFilterSearchV1(req, res);
+    });
+    this.router.post("/llm/groqv2-filter", async (req, res) => {
+      await this.groqFilterSearchV2(req, res);
+    });
+    this.router.post("/llm/hfv1", async (req, res) => {
+      await this.hfSearchV1(req, res);
+    });
+    this.router.post("/llm/hfv2", async (req, res) => {
+      await this.hfSearchV2(req, res);
+    });
+    // Define a rota POST para ingestão das ementas
+    this.router.post("/llm/hfv1-filter", async (req, res) => {
+      await this.hfFilterSearchV1(req, res);
+    });
+    this.router.post("/llm/hfv2-filter", async (req, res) => {
+      await this.hfFilterSearchV2(req, res);
     });
   }
 }
